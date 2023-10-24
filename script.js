0ee238ea52acf549e8d610a08fc255ec33665d39
André Dias Da Silva <andre75008@gmail.com>
2023-10-24 15:11:08 +0200

    function tri

--- script.js
+++ script.js
@@ -85,6 +85,9 @@ function productsWithoutError(productsArray) {
 }
 
 const sanitizeProductsList = productsWithoutError(products);
+let produitsSansErreur = productsWithoutError(products);
+
+
 
 sanitizeProductsList.forEach((product) => {
   //creer une colonne
@@ -136,10 +139,22 @@ sanitizeProductsList.forEach((product) => {
   btnCart.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
   <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
   <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-</svg>`;
+  </svg>`;
   cardBody.appendChild(btnCart);
 
   divCol.appendChild(divCard);
 
   productListContainer.appendChild(divCol);
+});
+
+// fonction tri
+const btn_tri = document.querySelector("#tri");
+btn_tri.addEventListener("click", function () {
+  console.log("coucou");
+  produitsSansErreur.sort((prix_indice_actuel, prix_indice_plus_un) => {
+    console.log("coucou2");
+    if (prix_indice_actuel.price > prix_indice_plus_un.price) return -1;
+    if (prix_indice_actuel.price < prix_indice_plus_un.price) return 1;
+    return 0;
+  });
 });
