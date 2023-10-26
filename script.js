6343456a9970112b30159c2747ede907f050e538
Yovann AUDINET <yovann.audinet@orange.fr>
2023-10-26 13:17:49 +0200

    Refactor du code

--- script.js
+++ script.js
@@ -19,6 +19,41 @@ async function fetcher(link) {
   return data;
 }
 
+prod = [];
+
+to_display_products = [];
+function clean_products_card() {
+  let card = $(".card_parent");
+  console.log(card);
+  card.remove();
+}
+function display_products_card() {
+  to_display_products.forEach((product) => {
+    //creer une colonne
+    let divCol = document.createElement("div");
+    divCol.classList.add("col");
+    divCol.classList.add("card_parent");
+
+    divCol.innerHTML = `
+<div class="card shadow border-0">
+   <img class="card-img-top" src='${product.img}'/>
+    <div class="card-body">
+      <h5 class="card-title">${product.title}</h5>
+      <p class="card-text">${product.description}</p>
+      <p class="fw-bold" >${product.price}€</p>
+      <button id="addToCart" class="btn position-absolute bottom-0 end-0">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
+          <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
+          <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+        </svg>
+      </button>
+    </div>
+</div>`;
+
+    productListContainer.appendChild(divCol);
+  });
+}
+
 fetcher(url)
   .then((products) => {
     //function pour filtrer les produits sans propriété ou erronés
@@ -72,31 +107,20 @@ fetcher(url)
       return productslist;
     }
 
-    const sanitizeProductsList = productsWithoutError(products);
-
-    sanitizeProductsList.forEach((product) => {
-      //creer une colonne
-      let divCol = document.createElement("div");
-      divCol.classList.add("col");
-
-      divCol.innerHTML = `
-  <div class="card shadow border-0">
-     <img class="card-img-top" src='${product.img}'/>
-      <div class="card-body">
-        <h5 class="card-title">${product.title}</h5>
-        <p class="card-text">${product.description}</p>
-        <p class="fw-bold" >${product.price}€</p>
-        <button id=""addToCart class="btn position-absolute bottom-0 end-0">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
-            <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
-            <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-          </svg>
-        </button>
-      </div>
-  </div>`;
-
-      productListContainer.appendChild(divCol);
+    const searchBar = document.querySelector("#searchBar");
+    searchBar.addEventListener("keyup", (e) => {
+      clean_products_card();
+      to_display_products = prod.filter((element) => {
+        return element.title.startsWith(e.target.value);
+      });
+      display_products_card();
     });
+    const sanitizeProductsList = productsWithoutError(products);
+    prod = sanitizeProductsList;
+
+    to_display_products = prod;
+    display_products_card();
+
     $(".card").animate(
       {
         opacity: 1,
